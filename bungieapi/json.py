c46d5b6cba13975cb402e7f03074ea24c76dde20
Damian Świstowski <damian@swistowski.org>
2021-12-25 02:37:32 +0000

    make linter happy

--- bungieapi/json.py
+++ bungieapi/json.py
@@ -16,20 +16,20 @@ def to_json(obj: t.Any) -> t.Any:
 
 
 @ft.singledispatch
-def to_json_for_simple(obj: t.Any):
+def to_json_for_simple(obj: t.Any) -> t.Any:
     return obj
 
 
 @to_json_for_simple.register
-def to_json_array(xs: list):
+def to_json_array(xs: list) -> t.Sequence:
     return [to_json(x) for x in xs]
 
 
 @to_json_for_simple.register
-def to_json_dict(obj: dict):
+def to_json_dict(obj: dict) -> t.Mapping:
     return {k: to_json(v) for k, v in obj}
 
 
 @to_json_for_simple.register
-def to_json_enum(obj: Enum):
+def to_json_enum(obj: Enum) -> t.Any:
     return to_json(obj.value)
