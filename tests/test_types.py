394dd100002d8a896852a62216857374c07a8ffe
Damian Świstowski <damian@swistowski.org>
2022-01-20 00:02:12 +0000

    keep information about reference type after structure (#19)

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,4 +1,8 @@
+import dataclasses as dt
+
+from bungieapi.forge import forge
 from bungieapi.generated.components.schemas.user import OptInFlags
+from bungieapi.types import ManifestReference
 
 
 def test_can_create_bitmask():
@@ -12,3 +16,33 @@ def test_can_decompose_type():
     assert {OptInFlags.USER_RESEARCH, OptInFlags.PLAY_TESTS} == set(
         OptInFlags.USER_RESEARCH | OptInFlags.PLAY_TESTS
     )
+
+
+def test_manifest_reference_can_be_structured():
+    @dt.dataclass
+    class A:
+        pass
+
+    @dt.dataclass
+    class B:
+        a: ManifestReference[A]
+
+    forged = forge(B, {"a": "42"})
+    assert forged.a.type == A
+    assert forged.a.hash == "42"
+
+
+def test_manifest_reference_can_be_structured_with_forward_ref():
+    @dt.dataclass
+    class A:
+        pass
+
+    globals()["A"] = A
+
+    @dt.dataclass
+    class B:
+        a: ManifestReference["A"]
+
+    forged = forge(B, {"a": "42"})
+    assert forged.a.type == A
+    assert forged.a.hash == "42"
